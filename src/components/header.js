727da6836c3642e19db58f772eafa6df555ff194
David Osemwegie <osahonnn@gmail.com>
2020-05-02 20:44:08 -0600

    learned how to use styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,51 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
 import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={require("../images/logo-designcode.svg")} width="30" />
-      </Link>
-      <Link to="">Courses</Link>
-      <Link to="">Downloads</Link>
-      <Link to="">Workshops</Link>
-      <Link to="">
-        <button>Buy</button>
-      </Link>
-    </div>
-  </div>
-)
+class Header extends Component {
+  constructor(props) {
+    super(props)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    this.state = {
+      hasScrolled: false,
+    }
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  //How to change the ccolor of the header if the user has scrolled
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={require("../images/logo-designcode.svg")} width="30" />
+          </Link>
+          <Link to="">Courses</Link>
+          <Link to="">Downloads</Link>
+          <Link to="">Workshops</Link>
+          <Link to="">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
