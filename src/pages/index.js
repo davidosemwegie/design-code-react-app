434d03ecabf3350de993964b3d4b1d3f53143f05
David Osemwegie <osahonnn@gmail.com>
2020-05-02 21:32:41 -0600

    added some static json

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,30 @@ import SEO from "../components/seo"
 import Card from "../components/Card"
 import Section from "../components/section"
 import Wave from "../components/wave"
+import staticdata from "../../staticdata.json"
+import Cell from "../components/cell"
+import styled from "styled-components"
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94a4ba;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 640px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 const IndexPage = () => (
   <Layout>
@@ -29,38 +53,46 @@ const IndexPage = () => (
           <img src={require("../images/logo-swift.png")} width="50px" />
         </div>
       </div>
-      <div className="Cards">
-        <h2>12 courses, more coming.</h2>
-        <div className="CardGroup">
-          <Card
-            title="Design Systems"
-            text="10 sections"
-            image={require("../images/wallpaper.jpg")}
-          />
-          <Card
-            title="React for Designers"
-            text="12 sections"
-            image={require("../images/wallpaper2.jpg")}
-          />
-          <Card
-            title="Sound Design"
-            text="5 sections"
-            image={require("../images/wallpaper3.jpg")}
-          />
-          <Card
-            title="ARKit 2"
-            text="12 sections"
-            image={require("../images/wallpaper3.jpg")}
-          />
-        </div>
-      </div>
-      <Section
-        image={require("../images/wallpaper2.jpg")}
-        logo={require("../images/logo-react.png")}
-        title="React for Designers"
-        text="Learn how to build a modern site suing React and the most efficient libraries, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with netlify"
-      />
+      <Wave />
     </div>
+    <div className="Cards">
+      <h2>12 courses, more coming.</h2>
+      <div className="CardGroup">
+        <Card
+          title="Design Systems"
+          text="10 sections"
+          image={require("../images/wallpaper.jpg")}
+        />
+        <Card
+          title="React for Designers"
+          text="12 sections"
+          image={require("../images/wallpaper2.jpg")}
+        />
+        <Card
+          title="Sound Design"
+          text="5 sections"
+          image={require("../images/wallpaper3.jpg")}
+        />
+        <Card
+          title="ARKit 2"
+          text="12 sections"
+          image={require("../images/wallpaper3.jpg")}
+        />
+      </div>
+    </div>
+    <Section
+      image={require("../images/wallpaper2.jpg")}
+      logo={require("../images/logo-react.png")}
+      title="React for Designers"
+      text="Learn how to build a modern site suing React and the most efficient libraries, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with netlify"
+    />
+
+    <SectionCaption>12 Sections - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell title={cell.title} image={cell.image} />
+      ))}
+    </SectionCellGroup>
   </Layout>
 )
 
